730d6b69e35471747f54633d23e2e52761808bea
shubham <shubham@itday.in>
2023-03-25 10:59:52 +0530

    notes update & delete done

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -40,7 +40,63 @@ const createNewNote = asyncHandler(async (req, res) => {
   }
 });
 
+const updateNote =asyncHandler(
+  async(req,res) => {
+    const {id,user,title,text ,completed} = req.body
+    if(!id || !title || !text || !user || typeof completed !== "boolean"){
+      return res.status(400).json({message:"All field req"})
+    }
+  
+    const note = await Notes.findById(id).exec()
+     if(!note){
+      return res.status(400).json({message:"Note not found"})
+     }
+
+     const duplicate = await Notes.findOne({title}).lean().exec()
+
+     if(duplicate && duplicate?._id.toString() !== id){
+      return res.status(409).json({ message: 'Duplicate note title' })
+     }
+
+     note.user = user
+     note.title = title
+     note.text = text
+     note.completed = completed
+
+     const updateNote = await note.save()
+
+     res.json(`${updateNote.title} updated`)
+
+  }
+) 
+
+
+const deleteNote = asyncHandler (
+
+  async(req,res) => {
+   const {id} = req.body
+   if (!id){
+    return res.status(400).json({message:"Note id req"})
+   }
+
+
+   const note = await Notes.findById(id).exec()
+
+   if(!note) {
+    return res.status(400).json({message:"Note not found"})
+   }
+
+   const result = await note.deleteOne()
+
+   const reply = `Note ${result.title} is deleted`
+
+   res.json(reply)
+   
+  }
+)
+
 module.exports = {
   getAllNotes,
-  createNewNote
+  createNewNote,
+  updateNote,deleteNote
 };
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,7 +1,15 @@
-const express = require("express")
-const router = express.Router()
-const notesController = require("../controllers/notesController")
-const verifyJWT = require("../middleware/verifyJWT")
+const express = require("express");
+const router = express.Router();
+const notesController = require("../controllers/notesController");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.use(verifyJWT)
-router.route("/").get(notesController.getAllNotes).post(notesController.createNewNote)
+router.use(verifyJWT);
+router
+  .route("/")
+  .get(notesController.getAllNotes)
+  .post(notesController.createNewNote)
+  .patch(notesController.updateNote)
+  .delete(notesController.deleteNote);
+
+
+  module.exports = router
